a42af7a2f1ef37ab1a6bb822d06f46fb0795e670
Christian Brassat <cbrst@niflheim.brassat.eu>
2024-12-30 12:47:10 +0100

    NVIM: add feline colors to rose-pine

--- nvim/lua/plugins/theme/rose-pine.lua
+++ nvim/lua/plugins/theme/rose-pine.lua
@@ -1,9 +1,6 @@
 return {
 	"rose-pine/neovim",
 	priority = 1000,
-	init = function()
-		vim.cmd.colorscheme("rose-pine")
-	end,
 	config = function()
 		require("rose-pine").setup({
 			variant = "auto",
@@ -20,59 +17,25 @@ return {
 				italic = true,
 				transparency = false,
 			},
-
-			highlight_groups = {
-				FlnBlue = { fg = "pine" },
-				FlnCyan = { fg = "foam" },
-				FlnMagenta = { fg = "iris" },
-				FlnRed = { fg = "love" },
-				FlnYellow = { fg = "gold" },
-				FlnBlueAlt = { fg = "pine", bg = "highlight_med" },
-				FlnCyanAlt = { fg = "foam", bg = "highlight_med" },
-				FlnMagentaAlt = { fg = "iris", bg = "highlight_med" },
-				FlnRedAlt = { fg = "love", bg = "highlight_med" },
-				FlnYellowAlt = { fg = "gold", bg = "highlight_med" },
-
-				FlnStatus = { bg = "base", fg = "muted" },
-				FlnStatusAlt = { bg = "muted", fg = "surface" },
-				FlnStatusBg = { bg = "text", fg = "surface" },
-
-				FlnViBlue = { bg = "pine", fg = "highlight_med" },
-				FlnViCyan = { bg = "foam", fg = "highlight_med" },
-				FlnViMagenta = { bg = "iris", fg = "highlight_med" },
-				FlnViRed = { bg = "love", fg = "highlight_med" },
-				FlnViYellow = { bg = "gold", fg = "highlight_med" },
-
-				FlnViBlueSep = { bg = "pine", fg = "text" },
-				FlnViCyanSep = { bg = "foam", fg = "text" },
-				FlnViMagentaSep = { bg = "iris", fg = "text" },
-				FlnViRedSep = { bg = "love", fg = "text" },
-				FlnViYellowSep = { bg = "gold", fg = "text" },
-
-				FlnAlt = { bg = "base", fg = "text" },
-				FlnSep = { bg = "base", fg = "text" },
-				FlnSepBgAlt = { bg = "muted", fg = "text" },
-				FlnSepAltDefault = { bg = "base", fg = "muted" },
-				-- FlnAltSep = { bg = "overlay", fg = "base" },
-
-				-- LineNr = { bg = "overlay", fg = "muted" },
-				-- FoldColumn = { link = "LineNr" },
-				-- SignColumn = { link = "LineNr" },
-
-				NeoTreeNormal = { bg = "surface" },
-				NeoTreeNormalNC = { bg = "surface" },
-				NeoTreeVertSplit = { bg = "surface" },
-
-				TelescopeBorder = { fg = "overlay", bg = "overlay" },
-				TelescopeNormal = { fg = "subtle", bg = "overlay" },
-				TelescopeSelection = { fg = "text", bg = "highlight_med" },
-				TelescopeSelectionCaret = { fg = "love", bg = "highlight_med" },
-				TelescopeTitle = { fg = "base", bg = "love" },
-				TelescopePromptTitle = { fg = "base", bg = "pine" },
-				TelescopePreviewTitle = { fg = "base", bg = "iris" },
-				TelescopePromptNormal = { fg = "text", bg = "surface" },
-				TelescopePromptBorder = { fg = "surface", bg = "surface" },
-			},
 		})
+
+		Myvi.feline_themes["rose-pine"] = {
+			light = {
+				bg0 = "#faf4ed",
+				bg1 = "#9993a7",
+				bg2 = "#575279",
+				fg0 = "#9893a5",
+				fg1 = "#faf4ed",
+				fg2 = "#faf4ed",
+			},
+			dark = {
+				bg0 = "#191724",
+				bg1 = "#908caa",
+				bg2 = "#e0def4",
+				fg0 = "#908caa",
+				fg1 = "#191724",
+				fg2 = "#191724",
+			},
+		}
 	end,
 }
