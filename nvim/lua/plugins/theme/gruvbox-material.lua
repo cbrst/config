1e7be5f79ce3c785eef711774184c389c4193617
Christian Brassat <cbrst@niflheim.brassat.eu>
2024-12-30 13:15:35 +0100

    NVIM: switch to a different implementation of gruvbox-material
    This one makes it easier to overwrite the colors to get rid of the
    piss-colored light background.

--- nvim/lua/plugins/theme/gruvbox-material.lua
+++ nvim/lua/plugins/theme/gruvbox-material.lua
@@ -1,25 +1,40 @@
 return {
-	"sainnhe/gruvbox-material",
+	"f4z3r/gruvbox-material.nvim",
 	priority = 1000,
 	lazy = true,
-	init = function()
-		vim.g.gruvbox_material_transparent_background = 0
-		vim.g.gruvbox_material_foreground = "original"
-		vim.g.gruvbox_material_background = "hard"
-		vim.g.gruvbox_material_ui_contrast = "high"
-		vim.g.gruvbox_material_float_style = "bright"
-		vim.g.gruvbox_material_statusline_style = "material"
-		vim.g.gruvbox_material_cursor = "auto"
-		vim.g.gruvbox_material_transparent_background = 1
-		vim.g.gruvbox_enable_bold = 1
-		vim.g.gruvbox_enable_italic = 1
-	end,
 	config = function()
+		local colors = require("gruvbox-material.colors").get(vim.o.background, "hard")
+
+		require("gruvbox-material").setup({
+			contrast = "hard",
+			customize = function(_, o)
+				if vim.o.background == "dark" then
+					return o
+				end
+
+				if o.bg == colors.bg0 then
+					o.bg = "#faf4ed"
+				elseif o.bg == colors.bg1 then
+					o.bg = "#f4eee9"
+				elseif o.bg == colors.bg2 then
+					o.bg = "#efe9e4"
+				elseif o.bg == colors.bg3 then
+					o.bg = "#e9e4e0"
+				elseif o.bg == colors.bg4 then
+					o.bg = "#e4dfdc"
+				elseif o.bg == colors.bg5 then
+					o.bg = "#dedbd7"
+				end
+
+				return o
+			end,
+		})
+
 		-- feline colors
 
 		Myvi.feline_themes["gruvbox-material"] = {
 			light = {
-				bg0 = "#fbf4ec",
+				bg0 = "#fbf4ed",
 				bg1 = "#a89984",
 				bg2 = "#3c3836",
 				fg0 = "#3c3836",
