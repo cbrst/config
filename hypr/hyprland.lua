10a921e7f48a9c568c73b4e9d0f30d544b4ff0f3
Christian Brassat <christian@brassat.eu>
2026-08-18 23:28:35 +0200

    feat: add layer rule for Noctalia with blur and animation settings

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -176,3 +176,14 @@ hl.window_rule({
     float = true,
     center = true,
 })
+
+hl.layer_rule({
+    name = "noctalia",
+    match = {
+        namespace = "^noctalia-(bar-.+|notification|dock|panel|attached-panel|osd|window-switcher)$",
+    },
+    no_anim = true,
+    ignore_alpha = 0.5,
+    blur = true,
+    blur_popups = true,
+})
